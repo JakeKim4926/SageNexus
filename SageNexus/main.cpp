724fab4f979fa7c568433515e1c2c629e2946033
김준섭 <wns1915@naver.com>
2026-03-22 18:18:26 +0900

    fix: WebView2 초기화 실패 수정 (CoInitializeEx STA 추가)

--- SageNexus/main.cpp
+++ SageNexus/main.cpp
@@ -11,8 +11,16 @@ int APIENTRY wWinMain(
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    if (!sageMgr.Initialize(hInstance))
+    // WebView2는 COM STA(Single-Threaded Apartment) 초기화 필요
+    HRESULT hrCom = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+    if (FAILED(hrCom))
         return -1;
+
+    if (!sageMgr.Initialize(hInstance))
+    {
+        CoUninitialize();
+        return -1;
+    }
 
     MainWindow mainWindow;
     if (!mainWindow.Create(nCmdShow))
@@ -29,5 +37,6 @@ int APIENTRY wWinMain(
     }
 
     sageMgr.Shutdown();
+    CoUninitialize();
     return static_cast<int>(msg.wParam);
 }

1415a596b7579eb8957fb94eb38e11f6a45b2cf5
Jake_Kim <wer1915@gmail.com>
2026-04-01 03:47:44 +0900

    feat: ExportService::ExportToWord() 추가

--- SageNexus/app/application/services/ExportService.h
+++ SageNexus/app/application/services/ExportService.h
@@ -8,4 +8,5 @@ public:
     BOOL ExportToCsv(const DataTable& table, const CString& strFilePath, const CString& strLang, CString& strError);
     BOOL ExportToXlsx(const DataTable& table, const CString& strFilePath, const CString& strLang, CString& strError);
     BOOL ExportToHtml(const DataTable& table, const CString& strFilePath, const CString& strLang, CString& strError);
+    BOOL ExportToWord(const DataTable& table, const CString& strFilePath, const CString& strLang, CString& strError);
 };

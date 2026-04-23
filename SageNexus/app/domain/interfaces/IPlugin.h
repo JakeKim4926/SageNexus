2df1a964c2671ab2c2105757439f31d60f8a9454
김준섭 <wns1915@naver.com>
2026-04-23 16:12:18 +0900

    refactor: IPlugin ABI 강화 - LPCWSTR 반환, LoadLibraryExW 보안 플래그 적용

--- SageNexus/app/domain/interfaces/IPlugin.h
+++ SageNexus/app/domain/interfaces/IPlugin.h
@@ -6,7 +6,7 @@ class IPlugin
 public:
     virtual ~IPlugin() {}
 
-    virtual int            GetAbiVersion() const = 0;
-    virtual const CString& GetPluginId() const   = 0;
-    virtual const CString& GetPluginName() const = 0;
+    virtual int     GetAbiVersion() const = 0;
+    virtual LPCWSTR GetPluginId()   const = 0;
+    virtual LPCWSTR GetPluginName() const = 0;
 };
